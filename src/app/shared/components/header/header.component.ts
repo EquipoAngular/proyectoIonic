2cbe4c66e2c8a69abe0ffc5beeed6c6f4a458f27
Luciano Pucciarelli <luciano.lcc@gmail.com>
2020-06-18 17:01:17 -0500

    Login/Infraestructura/Refactoring.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Platform } from '@ionic/angular';
+import { SecurityService } from '../../services/security.service';
 
 @Component({
   selector: 'app-header',
@@ -9,15 +10,20 @@ import { Platform } from '@ionic/angular';
 export class HeaderComponent implements OnInit {
   @Input() title: string;
   backButtonText = '';
-  
+
   constructor(
-    private platform: Platform
+    private platform: Platform,
+    private securityService: SecurityService
   ) { }
 
   ngOnInit() {
     if (this.platform.is('ios')) {
       this.backButtonText = 'Volver';
     }
+  }
+
+  logoff() {
+    this.securityService.Logoff();
   }
 
 }
